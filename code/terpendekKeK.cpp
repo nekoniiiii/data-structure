78edbdb9acff2bc9a101e93e7d5357f896509c6a
Neisa Hibatillah <72701806+nekoniiiii@users.noreply.github.com>
2021-07-13 17:09:47 +0700

    Update and rename terpendekKeK_5025201170.cpp to terpendekKeK.cpp

---
+++ code/terpendekKeK.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int jumlah, urutan;
+    	cin >> jumlah >> urutan;
+ 
+    	int arr[jumlah];
+ 
+    	for(int i=0; i<jumlah; i++) cin >> arr[i];
+		int n = sizeof(arr) / sizeof(arr[0]);
+	
+	sort(arr, arr+n);
+    	cout << arr[urutan-1];
+    
+    	return 0;
+}
